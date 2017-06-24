7e2f8aba32016c8ccf0003892041b05a51fd315e
justBorn <alessandro.francia@gmail.com>
2017-06-24 18:13:45 +0100

    switchung to websocket

---
+++ examples/chat/index2.js
@@ -0,0 +1,56 @@
+var http = require("http");
+var ws = require("nodejs-websocket");
+var fs = require("fs");
+
+http.createServer(function (req, res) {
+    fs.createReadStream("index.html").pipe(res)
+}).listen(8080);
+
+var conversation = {};
+
+
+var server = ws.createServer(function (connection) {
+    var addedUser = false;
+    connection.on("text", function (str) {
+
+
+        str = JSON.parse(str);
+        switch (str.event) {
+            case 'new message' :
+                var data = str.data
+                conversation[data.conversationId] = conversation[data.conversationId] || [];
+                conversation[data.conversationId].push({
+                    username: data.username,
+                    message: data.message
+                });
+                break;
+
+                broadcast({
+                    event: 'new message',
+                    data: {
+                        username: str.data.username,
+                        message: str.data.message,
+                        conversationId: str.data.conversationId
+                    }
+                })
+        }
+
+
+        // if (connection.nickname === null) {
+        //     connection.nickname = str
+        //     broadcast(str + " entered")
+        // } else
+        //     broadcast("[" + connection.nickname + "] " + str)
+    });
+    connection.on("close", function () {
+        broadcast(connection.nickname + " left")
+    })
+});
+server.listen(3000);
+
+function broadcast(str) {
+    str = JSON.stringify(str);
+    server.connections.forEach(function (connection) {
+        connection.sendText(str)
+    })
+}
